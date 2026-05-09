a65d1cff46125721c2627d55f6ff0b52a71315c4
Valerio Grasso <valeriograsso04@gmail.com>
2026-05-09 22:14:19 +0200

    Navbar

--- AstraNote-Client/src/components/Navbar.jsx
+++ AstraNote-Client/src/components/Navbar.jsx
@@ -1,9 +1,73 @@
-const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  );
-};
+import { useState } from 'react'
+import flagIT from "../assets/flag-italy.svg"
+import flagUK from "../assets/flag-uk.svg"
+import themeIcon from "../assets/dark-theme.svg"
+import searchIcon from "../assets/search.svg"
+import libraryIcon from "../assets/library-1.svg"
+import profileIcon from "../assets/profile.svg"
+import settingsIcon from "../assets/settings.svg"
+import reactLogo from "../assets/react.svg"
+import "./Navbar.css"
+
+const translations = {
+            it: {
+                login: "Accedi",
+                register: "Registrati",
+                settings: "Impostazioni",
+                profile: "Profilo",
+                library: "La tua Libreria",
+                search: "Cerca degli appunti...",
+                terms: "Termini e Condizioni",
+                footer: "AstraNote 2026 - Tutti i diritti riservati"
+            },
+            en: {
+                login: "Login",
+                register: "Create Account",
+                settings: "Settings",
+                profile: "Profile",
+                library: "Your Library",
+                search: "Look for some notes...",
+                terms: "Terms and Conditions",
+                footer: "AstraNote 2026 - All rights reserved"
+              }
+        };
+
+function Navbar({children}) {
+    const [lang, setLang] = useState('it');
+
+    const cambiaLingua = () => {setLang(lang === 'it' ? 'en' : 'it')};
+
+    return (
+        <>
+          <nav className="navbarCnt">
+            <div className="logoCnt">
+                <img src={reactLogo} className="logo" alt="React logo"></img>
+            </div>
+            <div className="flagCnt">
+                <img src={lang == 'it' ? flagIT : flagUK} alt="change lang" className="flag" onClick={cambiaLingua}></img>
+            </div>
+            <div className="temaCnt">
+                <img src={themeIcon} alt="tema" className="tema"></img>
+            </div>
+            <div className="searchCnt">
+                <img src={searchIcon} className="searchIcon" alt="searchIcon" />
+                <input className="search" placeholder={translations[lang].search}></input>
+            </div>
+            <div className="libraryCnt">
+                <img src={libraryIcon} className="libraryIcon" alt="libraryIcon"></img>
+                <a href="#" className="library">{translations[lang].library}</a>
+            </div>
+            <div className="profileCnt">
+                <img src={profileIcon} alt="profile" className="profileIcon"></img>
+                <a href="#" className="profile">{translations[lang].profile}</a>
+            </div>
+            <div className="settingsCnt">
+                <img src={settingsIcon} alt="settings" className="settingsIcon"></img>
+                <a href="#" className="settings">{translations[lang].settings}</a>
+            </div>
+          </nav>
+        </>
+    )
+}
 
 export default Navbar;
