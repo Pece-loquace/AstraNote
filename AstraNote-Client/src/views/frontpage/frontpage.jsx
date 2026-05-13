222a0358d35b2d4d2d93cd6083ba890d61d089b8
Valerio Grasso <valeriograsso04@gmail.com>
2026-05-13 15:16:07 +0200

    frontpage unico

--- AstraNote-Client/src/views/frontpage/frontpage.jsx
+++ AstraNote-Client/src/views/frontpage/frontpage.jsx
@@ -1,44 +1,61 @@
-import {Link} from "react-router-dom"
+import { useState} from 'react';
+import RegistrazioneConFacolta from '../register/Register'
 import '../../style/sfondo.css'
 import '../../style/buttons.css'
+import './frontpage.css'
 import logo from "../../../public/AstraNote_Banner_IT.png"
 
 
 const Frontpage = () =>{
+    const [view, setView] = useState('frontpage');
     return (
         <>
         <div className="sfondo"></div>
         <div id="overlay"></div>
-        <div className="container">
-            <img src={logo} alt="logo" className="w-1"
-                    style={{ 
-                        backgroundSize: '60%',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center',
-                        width: '70%',
-                        height: '70%'
-                        }}/>
-        <div className="container mt-5">
-            <div className="d-flex gap-5 justify-content-center">
-                <Link to="/login">
-                    <button className="btn-custom " style={{width: '260px', height: '70px', fontSize: '32px'}}>
-                        Accedi
-                    </button>
-                </Link>
+        <div className="scurisco"></div>
 
-                <Link to="/register">
-                    <button className="btn-custom" style={{width: '260px', height: '70px', fontSize: '32px'}}>
-                        Registrati
-                    </button>
-                </Link>
-
-                <Link to="/upload">
-                    <button className="btn-custom" style={{width: '260px', height: '70px', fontSize: '32px'}}>
-                        Upload
-                    </button>
-                </Link>
+        <div className={`cnt ${view}`}>
+            
+            <div className="panel left">
+                <div className="form-box" style={{width: '40vw'}}>
+                    <h2>Accedi</h2>
+                    <input type="text" placeholder="Email" />
+                    <button className="btn-custom" onClick={() => setView('frontpage')}>Annulla</button>
+                </div>
             </div>
-        </div>
+
+            <div className="frontpage">
+                <img src={logo} alt="logo" className="logo"/>
+                <div className="buttons">
+                    <div className="d-flex gap-5 justifyContent-center">
+                        {view !== 'login' && (
+                        <>
+                        <button className="btn-custom" style={{width: '260px', height: '70px', fontSize: '32px'}}
+                                onClick={() => setView('login')}>
+                            Accedi
+                        </button>
+                        {view !== 'frontpage' && (
+                            <button className="btn-custom_2" style={{width: '260px', height: '70px', fontSize: '32px'}}
+                                    onClick={() => setView('frontpage')}>
+                                Indietro
+                            </button>
+                        )}
+                        </>
+                        )}                            
+                        
+                        {view !== 'register' && (
+                        <button className="btn-custom" style={{width: '260px', height: '70px', fontSize: '32px'}}
+                                onClick={() => setView('register')}>
+                            Registrati
+                        </button>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            <div className="panel right">
+                <RegistrazioneConFacolta className="form-box" style={{width: '40vw'}}/>
+            </div>
         </div>
         </>
     );
