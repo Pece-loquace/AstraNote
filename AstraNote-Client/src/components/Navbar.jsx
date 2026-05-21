35d691f8c050aec0c15b63c02f1969a17ca52adf
Valerio Grasso <valeriograsso04@gmail.com>
2026-05-21 19:14:45 +0200

    hamburger 1.0

--- AstraNote-Client/src/components/Navbar.jsx
+++ AstraNote-Client/src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import libraryIcon from "../assets/library-1.svg"
 import profileIcon from "../assets/profile.svg"
 import settingsIcon from "../assets/settings.svg"
 import AstraLogo from "../assets/AstraNote_Logo.svg"
-import ReturnButton from "../assets/return-button.svg"
+import logo from "../assets/AstraNote_Banner_IT_720.png"
 import "../style/Navbar.css"
 import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar({children}) {
     const [query,setQuery] = useState('');
-    const [menu, setMenu] = useState(false);
     const navigate = useNavigate();
+    const [menu, setMenu] = useState(false);
 
-      const handleKeyDown = (e) => {    
+    const handleKeyDown = (e) => {    
          if (e.key === "Enter" && query.trim() !== " ") {
             navigate(`/search?q=${encodeURIComponent(query)}`);
         }
@@ -34,10 +34,47 @@ function Navbar({children}) {
              console.error("Errore di rete:", error);
         }
     }
+
+    const Menu = ({isOpen, onClick}) =>{
+
+        return (
+            <div className={`menuCnt ${isOpen ? "open" : ""}`} onClick={onClick}>
+                <div className="menuCnt_child_1">
+                    <div className="menuCnt_child_2">
+                        <section className="menuSection">
+                            <div className="menuHeaderCnt">
+                                <div className="menuH_child_1">
+                                    <div className="menuH_child_2">
+                                        <img src={logo} alt="" className="menuLogo"/>
+
+                                        <div className="menuClose">
+                                            <button aria-label="close action" className="menuCloseBtn" onClick={() => setMenu(!menu)}>
+                                                <span className="menuCloseSvg">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256">
+                                                        <path d="M208.49,191.51a12,12,0,0,1-17,17L128,145,64.49,208.49a12,12,0,0,1-17-17L111,128,47.51,64.49a12,12,0,0,1,17-17L128,111l63.51-63.52a12,12,0,0,1,17,17L145,128Z"></path>
+                                                    </svg>
+                                                </span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr/>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        )
+    };
    
     return (
         <>
             <nav className="navbarCnt navbar-expand-lg navbar-expand-md navbar-expand-sm">
+
+                <div aria-label="menu action" className="settingsCntMobile">
+                    <img src={settingsIcon} alt="settings" className="settingsIcon" onClick={() => setMenu(!menu)}></img>   
+                </div>
+
                 <div className="logoCnt">
                     <Link to="/homepage">
                         <img src={AstraLogo} className="logo" alt="React logo" ></img>
@@ -69,31 +106,9 @@ function Navbar({children}) {
                         <a className="dropdown-item" id="logoutItem" onClick={()=>(Logout())} >Logout</a>
                     </div>
                 </div>
-
-                <div className="settingsCnt_mobile">
-                    <img src={settingsIcon} alt="settings" className="settingsIcon"
-                         onClick={() => setMenu(!menu)}></img>
-                </div>
             </nav>
 
-            <div className={`menu ${menu === true ? 'd-flex' : 'd-none d-md-flex'}`}>
-                <div className="libraryCnt">
-                    <img src={libraryIcon} className="libraryIcon" alt="libraryIcon"></img>
-                    <Link className="library" to="/libreria">La tua libreria</Link>
-                </div>
-
-                <div className="dropdown_mobile">
-                    <img src={profileIcon} alt="profile" className="profileIcon"></img>
-                    <button className="profile dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Profilo
-                    </button>
-                    <div className="dropdown-menu_mobile" aria-labelledby="dropdownMenuButton">
-                        <a className="dropdown-item action" href="/libreria">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item logout" id="logoutItem" onClick={()=>(Logout())} >Logout</a>
-                    </div>
-                </div>
-            </div>
+            {menu !== false && <Menu isOpen={menu} toggleMenu={() => setMenu(!menu)}></Menu>}
         </>
     )
 }
