e064363701907b412c014fd93924e552008a39dd
Alessandro-Corvi <alessandro.corvi3@gmail.com>
2026-05-26 16:25:17 +0200

    Rimuove testo in search

--- AstraNote-Client/src/views/searchpage/searchpage.jsx
+++ AstraNote-Client/src/views/searchpage/searchpage.jsx
@@ -7,7 +7,7 @@ import Filters from "../../components/Filters";
 export default function Searchpage(){
     const [appunti,setAppunti] = useState([])
     const[error,setError] = useState("")
-    const[loading,setLoading] = useState(false)
+    const[loading,setLoading] = useState(true)
     const[filters,setFilters] = useState({facolta:"",anno:"",stelle:""})
 
     /*Recupera il valore di q dall'URL */
