be6a4f5d4e515411772ef62397851878b4429877
SimoneP-Alteri <spalter96@gmail.com>
2026-05-27 17:29:17 +0200

    fix vari Mission e aggiunto contattaci

--- AstraNote-Client/src/App.jsx
+++ AstraNote-Client/src/App.jsx
@@ -3,7 +3,7 @@ import Homepage from "./views/homepage/Homepage"
 import DefaultLayout from "./views/Layout/DefaultLayout"
 import AppuntoSpecifico from './components/AppuntoSpecifico'
 
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UploadNota from "./views/uploadnota/UploadNota"
 import Searchpage from "./views/searchpage/searchpage"
 import Libreria from "./views/libreria/Libreria"
@@ -13,6 +13,7 @@ import Impostazioni from "./views/impostazioni/Impostazioni"
 import ModificaAppunto from "./components/ModificaAppunto";
 import Mission from "./views/Mission/Mission"
 import Valori from "./views/Valori/Valori"
+import Contattaci from "./views/contattaci/Contattaci"
 
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -21,7 +22,7 @@ export default function App() {
 
   return (
     <BrowserRouter>
-    <ScrollToTop />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<RedirectIfLoggedIn><Frontpage /></RedirectIfLoggedIn>} />
 
@@ -31,11 +32,12 @@ export default function App() {
           <Route path="/search" element={<Searchpage />} />
           <Route path="/libreria" element={<Libreria />} />
           <Route path="/upload" element={<UploadNota />} />
-          <Route path="/utente/:id" element ={<Utente/>}/>
-          <Route path="/impostazioni" element={<Impostazioni/>}/>
-          <Route path="/modifica/:id" element = {<ModificaAppunto/>}/>
-          <Route path="/La_nostra_Mission" element = {<Mission/>}/>
-          <Route path="/I_nostri_Valori" element = {<Valori/>}/>
+          <Route path="/utente/:id" element={<Utente />} />
+          <Route path="/impostazioni" element={<Impostazioni />} />
+          <Route path="/modifica/:id" element={<ModificaAppunto />} />
+          <Route path="/La_nostra_Mission" element={<Mission />} />
+          <Route path="/I_nostri_Valori" element={<Valori />} />
+          <Route path="/Contattaci" element={<Contattaci />} />
         </Route>
       </Routes>
     </BrowserRouter>
