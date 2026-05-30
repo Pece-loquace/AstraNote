cbd15911cd1ab7f438472a0f6cd1c5d83ecea74b
Valerio Grasso <valeriograsso04@gmail.com>
2026-05-30 16:30:01 +0200

    Eliminazione file non utilizzati

--- AstraNote-Server/server2/src/routes/index.js
+++ AstraNote-Server/server2/src/routes/index.js
@@ -3,7 +3,6 @@ const segnalazioniRoutes = require("./segnalazioni.routes");
 const recensioniRoutes = require("./recensioni.routes");
 const catalogoRoutes = require("./catalogo.routes");
 const preferitiRoutes = require("./preferiti.routes");
-const appuntiFetchRoutes = require("./appunti.fetch.routes");
 const utentiRoutes = require("./utenti.routes");
 const authRoutes = require("./auth.routes");
 
@@ -14,7 +13,6 @@ function registerRoutes(app) {
   app.use(recensioniRoutes);
   app.use(catalogoRoutes);
   app.use(preferitiRoutes);
-  app.use(appuntiFetchRoutes);
   app.use(utentiRoutes);
   app.use(authRoutes);
 }
